0cb5286da0b6643af71d29d6c89135c894b85338
aitikirala <aitikira@terpmail.umd.edu>
2023-12-31 02:09:45 -0500

    created the calendar component, tried my best

--- app/TeacherWelcomeScreen.js
+++ app/TeacherWelcomeScreen.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
+import { Calendar } from 'react-native-calendars';
 import { supabase } from "./supabase";
 
 const TeacherWelcomeScreen = () => {
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [selectedDate, setSelectedDate] = useState('');
 
   useEffect(() => {
     const fetchProfile = async () => {
@@ -31,6 +33,11 @@ const TeacherWelcomeScreen = () => {
     fetchProfile();
   }, []);
 
+  const onDayPress = (day) => {
+    setSelectedDate(day.dateString);
+    // Here you can handle any side effects of changing the date.
+  };
+
   if (loading) {
     return <ActivityIndicator />;
   }
@@ -38,6 +45,23 @@ const TeacherWelcomeScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Welcome, {profile?.first_name}!</Text>
+
+      <Calendar
+        style={styles.calendarStyle}
+        theme={{
+          'stylesheet.calendar.header': {
+            week: {
+              marginTop: 5,
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }
+          }
+        }}
+        onDayPress={onDayPress}
+        markedDates={{
+          [selectedDate]: {selected: true, marked: true, selectedColor: 'blue'},
+        }}
+      />
     </View>
   );
 };
@@ -45,13 +69,22 @@ const TeacherWelcomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
+    marginTop: 100,
   },
   welcomeText: {
     fontSize: 20,
     fontWeight: "bold",
+    marginBottom: 20,
   },
+  calendarStyle: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: 350,
+    height: 400 
+  },
+  
 });
 
 export default TeacherWelcomeScreen;
